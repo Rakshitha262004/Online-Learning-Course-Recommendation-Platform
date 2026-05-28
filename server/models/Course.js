e30739b26a2ca9994b8551e4b27b6f74d5482a6e
Rakshitha262004 <rakshithaas2004@gmail.com>
2026-05-28 14:48:23 +0530

    Add files via upload

---
+++ server/models/Course.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    tags: [String]
+
+});
+
+module.exports = mongoose.model("Course", courseSchema);
